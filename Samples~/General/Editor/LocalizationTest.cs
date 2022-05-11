d1550a86c1b9a15b9b5d8b22c3545865ad0326d7
anatawa12 <anatawa12@icloud.com>
2022-05-11 16:00:59 +0900

    add sample

---
+++ Samples~/General/Editor/LocalizationTest.cs
@@ -0,0 +1,32 @@
+using CustomLocalization4EditorExtension;
+using UnityEditor;
+
+namespace CustomLocalization4EditorExtension.Sample.General
+{
+    public class LocalizationTest : EditorWindow
+    {
+        [MenuItem("CustomLocalization4EditorExtension/LocalizationTest")]
+        public static void Open()
+        {
+            GetWindow<LocalizationTest>();
+        }
+
+        private Localization _local = new Localization("d5c0fdcf0b524fb6ba5332337e2eecfb", "en");
+        private string _input;
+
+        private void OnGUI()
+        {
+            _local.DrawLanguagePicker();
+            _input = EditorGUILayout.TextField("key", _input);
+            if (_input != null)
+            {
+                EditorGUILayout.LabelField(_local.Tr(_input));
+            }
+        }
+
+        private void OnEnable()
+        {
+            _local.Setup();
+        }
+    }
+}
