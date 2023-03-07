6680ceee51e7988d657c91efa9d8c312aadc3dd6
anatawa12 <anatawa12@icloud.com>
2023-03-07 21:10:00 +0900

    chore: surround with #if UNITY_EDITOR

--- Localization.cs
+++ Localization.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -419,3 +420,4 @@ namespace CustomLocalization4EditorExtension
         }
     }
 }
+#endif 
