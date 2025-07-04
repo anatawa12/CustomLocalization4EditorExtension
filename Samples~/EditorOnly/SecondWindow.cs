ecb7e85b5a90e02bdbe73d807b97fe485152a1e3
anatawa12 <anatawa12@icloud.com>
2025-07-04 17:01:16 +0900

    chore: update samples

--- Samples~/EditorOnly/SecondWindow.cs
+++ Samples~/EditorOnly/SecondWindow.cs
@@ -3,20 +3,22 @@ using UnityEditor;
 
 namespace CustomLocalization4EditorExtension.Sample.EditorOnly
 {
-    public class LocalizationTest : EditorWindow
+    public class SecondWindow : EditorWindow
     {
-        [MenuItem("CustomLocalization4EditorExtension/LocalizationTest EditorOnly")]
+        [MenuItem("CustomLocalization4EditorExtension/Samples/EditorOnly/Second Window")]
         public static void Open()
         {
-            GetWindow<LocalizationTest>();
+            GetWindow<SecondWindow>();
         }
 
-        [AssemblyCL4EELocalization]
-        private static Localization Localization { get; } = new Localization("9d07519d86254715b7e730d6731520b2", "en");
         private string _input;
+
+        // since [AssemblyCL4EELocalization] is defined in the FirstWindow class,
+        // you should not define it again in this class.
 
         private void OnGUI()
         {
+            // With CL4EE class, you can access the [AssemblyCL4EELocalization] instance from this class.
             CL4EE.DrawLanguagePicker();
             _input = EditorGUILayout.TextField(CL4EE.Tr("prop:key"), _input);
             if (_input != null)
